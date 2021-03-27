eb511334622d256138e220a520ece4f84269e24b
root <memarkemil@gmail.com>
2021-03-27 01:18:20 +0000

    feat: Used BooksAPI to fetch books & added 2 components also added routing functionality

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
+import ListBooks from './ListBooks'
+import AddBooks from './AddBooks'
+
+class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+
+componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) => {
+        this.setState(() => ({
+          books
+        }))
+      })
+  }
+
+  render() {
+    const { books } = this.state
+    return (
+      <div className="app">
+       <Route exact path='/' render={() => (
+    <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <div className="bookshelf-books">
+                    <ListBooks books={books} shelf="currentlyReading" />
+                  </div>
+                </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Want to Read</h2>
+                  <div className="bookshelf-books">
+                    <ListBooks books={books} shelf="wantToRead" />
+                  </div>
+                </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Read</h2>
+                  <div className="bookshelf-books">
+                    <ListBooks books={books} shelf="read" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="open-search">
+              <Link
+				to='/search'>
+				Add a book
+				</Link>
+            </div>
+          </div>
+        )}
+        />
+		<Route path='/search' render={() => (
+        <AddBooks />
+        )} />
+       
+      </div>
+    )
+  }
+}
+
+export default BooksApp
---
+++ src/ListBooks.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+class ListBooks extends Component {
+  render() {
+    const { books, shelf, onUpdateBookShelf } = this.props
+    
+  return (
+  <ol className="books-grid">
+    {books.filter((b) => (b.shelf.toLowerCase() === shelf.toLowerCase())).map((book) => (
+   		<li key={book.id}>
+    		<div className="book">
+    			<div className="book-top">
+                	<div className="book-cover" style={{
+    				width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
+					</div>
+					<div className="book-shelf-changer">
+						<select>
+							<option value="move" disabled>Move to...</option>
+                        	<option value="currentlyReading">Currently Reading</option>
+                        	<option value="wantToRead">Want to Read</option>
+                        	<option value="read">Read</option>
+                        	<option value="none">None</option>
+                        </select>
+                     </div>
+                  </div>
+                  <div className="book-title">{book.title}</div>
+                      <div className="book-authors">{book.authors}</div>
+                  </div>
+                      </li>
+  ))}
+                     
+                    </ol>
+  )
+  }
+}
+
+export default ListBooks
