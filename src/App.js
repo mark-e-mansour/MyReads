311d87f28107474bb050042cb10201771575892f
root <memarkemil@gmail.com>
2021-03-27 12:34:34 +0000

    feat: functionality for changing book category list added

--- src/App.js
+++ src/App.js
@@ -20,11 +20,23 @@ componentDidMount() {
       })
   }
 
+updateBookShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf)
+      .then((shelf) => {
+       BooksAPI.getAll()
+      .then((books) => {
+        this.setState(() => ({
+          books
+        }))
+      })
+      })
+  }
+
   render() {
     const { books } = this.state
     return (
       <div className="app">
-       <Route exact path='/' render={() => (
+       <Route exact path='/' render={({ history }) => (
     <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -34,19 +46,25 @@ componentDidMount() {
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
-                    <ListBooks books={books} shelf="currentlyReading" />
+                    <ListBooks books={books} shelf="currentlyReading" onUpdateBookShelf={(book, shelf) => {
+      				this.updateBookShelf(book, shelf)
+					}} />
                   </div>
                 </div>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
-                    <ListBooks books={books} shelf="wantToRead" />
+                    <ListBooks books={books} shelf="wantToRead" onUpdateBookShelf={(book, shelf) => {
+      				this.updateBookShelf(book, shelf)
+					}}/>
                   </div>
                 </div>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Read</h2>
                   <div className="bookshelf-books">
-                    <ListBooks books={books} shelf="read" />
+                    <ListBooks books={books} shelf="read" onUpdateBookShelf={(book, shelf) => {
+      				this.updateBookShelf(book, shelf)
+					}}/>
                   </div>
                 </div>
               </div>
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -14,7 +14,7 @@ class ListBooks extends Component {
     				width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
 					</div>
 					<div className="book-shelf-changer">
-						<select>
+						<select onChange={(event) => onUpdateBookShelf(book, event.target.value)} value={shelf}>
 							<option value="move" disabled>Move to...</option>
                         	<option value="currentlyReading">Currently Reading</option>
                         	<option value="wantToRead">Want to Read</option>
